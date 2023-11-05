fd3f3bf87e51435d2c874f2aa4db22efcbb07af3
Nizshime-Samara <samaramathildes@gmail.com>
2023-11-05 11:18:09 -0300

    Acrescentando a complexidade do algoritimo projeto4

--- AT/Matematica_Avancada/projeto4.py
+++ AT/Matematica_Avancada/projeto4.py
@@ -3,7 +3,8 @@ A função é planejada para tratar do caso geral(contar o número de ocorrênci
 verificando o valor atual do vetor e se necessário somando antes de fazer a chamada recursiva.
 Esta função usa a recursão como sua abordagem principal, onde a ideia por trás da recursão é dividir o 
 problema em subproblemas menores, resolvê-los individualmente e combinar seus resultados para obter a solução final 
-A classificação correta desse problema é P (Problemas Polinomiais) . 
+A classificação correta desse problema é P (Problemas Polinomiais) .
+Complexidade  O(n). 
 '''
 def conta_ocorrencias_recursivo(V, D, index=0):
     # Caso base: se o índice é igual ao tamanho do vetor, retornar 0
@@ -21,6 +22,7 @@ A função é planejada para tratar do caso geral(contar o número de ocorrênci
 a função iterativa conta_ocorrencias_iterativo usa uma estrutura de repetição 
 for para percorrer todo o vetor V.
 A classificação correta desse problema é P (Problemas Polinomiais) 
+Complexidade  O(n). 
 '''
 def conta_ocorrencias_iterativo(V, D):
     count = 0
